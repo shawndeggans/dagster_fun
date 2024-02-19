4c1ba21bba65d9f821f6d9689c58570aa912dbe6
Shawn Deggans <shawndeggans@gmail.com>
2024-02-19 17:54:40 +0000

    Learnig to use the built in dagster way of importing libraries

--- my-dagster-project/setup.py
+++ my-dagster-project/setup.py
@@ -5,7 +5,8 @@ setup(
     packages=find_packages(exclude=["my_dagster_project_tests"]),
     install_requires=[
         "dagster",
-        "dagster-cloud"
+        "dagster-cloud",
+        "pandas"
     ],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
